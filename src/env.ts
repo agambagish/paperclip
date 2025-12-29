3a9049459fc8a2903a59428a521830a10ae84775
AkashDotJS <akash06082006@gmail.com>
2025-12-29 19:00:46 +0530

    feat: add resend api key validation

--- src/env.ts
+++ src/env.ts
@@ -6,17 +6,21 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
-    NODE_ENV: z.enum(["development", "production"]).default("development"),
+    APP_ENV: z.enum(["development", "production"]).default("development"),
     BETTER_AUTH_SECRET: z.string(),
+    RESEND_API_KEY: z.string(),
+    EMAIL_FROM: z.string(),
   },
   client: {
     NEXT_PUBLIC_BASE_URL: z.url(),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
+    APP_ENV: process.env.APP_ENV,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    EMAIL_FROM: process.env.EMAIL_FROM,
   },
   emptyStringAsUndefined: true,
   onValidationError: (issues) => {
