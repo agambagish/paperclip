76a19d273aafceab83c82fb6cbb3e66c1f2dcf1d
AkashDotJS <akash06082006@gmail.com>
2025-12-30 12:02:12 +0530

    feat(auth): add necessary config changes

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 
 import { db } from "@/db";
 import { env } from "@/env";
@@ -26,5 +27,21 @@ export const auth = betterAuth({
         verifyUrl: url,
       });
     },
+  },
+  rateLimit: {
+    enabled: false,
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 1,
+    },
+  },
+  plugins: [nextCookies()],
+  logger: {
+    disabled: true,
+  },
+  advanced: {
+    cookiePrefix: "paperclip",
   },
 });
